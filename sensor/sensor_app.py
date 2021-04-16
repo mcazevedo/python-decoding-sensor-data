8359e935e77c67d194465b927fc9d81921515040
Matheus Azevedo <matheus.azevedo@qdois.com.br>
2021-04-16 19:55:46 -0300

    teste particulate

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -3,6 +3,8 @@ from load_data import load_sensor_data
 from house_info import HouseInfo
 from datetime import date, datetime
 from temperature_info import TemperatureData
+from humidity_info import HumidityData
+from statistics import mean
 
 ##############################
 # Do not remove these two lines
@@ -13,21 +15,20 @@ print("Sensor Data App")
 
 data = load_sensor_data()
 
-print("Loaded records: {} ".format(len(data)))
-
-
+# Instanciate the house_info object and assign values to variables
 house_info = HouseInfo(data)
 test_area = 1
 test_date = datetime.strptime("5/9/20", "%m/%d/%y")
 
+# Count the number of records to analyze
 recs = house_info.get_data_by_area("id", rec_area=test_area)
 print("\nHouse sensor records for area {} = {}" .format(test_area, len(recs)))
 
-
-
+# Filter records by 
 recs = house_info.get_data_by_date("id", rec_date=test_date)
 print("\nHouse sensor records for date {} = {}" .format(test_date.strftime("%m/%d/%y"), len(recs)))
 
+# Instantiate the temperature data object
 temperature_data = TemperatureData(data)
 
 recs = temperature_data.get_data_by_area(rec_area=test_area)
@@ -36,6 +37,16 @@ print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
 
 
 recs = temperature_data.get_data_by_date(rec_date=test_date)
-
 print("\nHouse Temperature sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
+
+# Instantiate the humidity object
+humidity_data = HumidityData(data)
+
+recs = humidity_data.get_data_by_area(rec_area=test_area)
+print("\nHouse Humidity sensor records for area {} = {}".format(test_area, len(recs)))
+print("\tAverage: {} humidity".format(mean(recs)))
+
+recs = humidity_data.get_data_by_date(rec_date=test_date)
+print("House Humidity sensor records for date: {} = {}".format( test_date.strftime("%m/%d/%y"), len(recs)))
+print("\tAverrage: {} humidity".format(mean(recs)))
