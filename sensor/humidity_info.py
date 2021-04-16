b0bc456771354865b242fdc742b9d5e188f4adde
Matheus Azevedo <matheus.azevedo@qdois.com.br>
2021-04-16 19:56:41 -0300

    teste particulate

--- sensor/humidity_info.py
+++ sensor/humidity_info.py
@@ -5,7 +5,7 @@ class HumidityData(HouseInfo):
     def _convert_data(self, data):
         recs = []
         for rec in data:
-            recs.append(float(rec))
+            recs.append(float(rec)*100)
         return recs
 
 
